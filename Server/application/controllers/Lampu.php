e5ea0d64f4cd7d6056826c6aa521db8d94b69900
4q1l <aqilalhafizh6@gmail.com>
2022-12-30 07:54:16 +0700

    master: perbaiki update mahasiswa

--- Server/application/controllers/Lampu.php
+++ Server/application/controllers/Lampu.php
@@ -1,110 +1,100 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
+// panggil file "Server.php"
+require APPPATH . "libraries/Server.php";
+// require APPPATH."libraries/Server.php";
 
-require APPPATH."libraries/Server.php";
+class Lampu extends Server
+{
 
-class Lampu extends Server {
-
-    //buat konstruktor
     public function __construct()
-        {
-                parent::__construct();
-                 //panggil model "Mlampu"
-                $this->load->model("Mlampu","model",TRUE);
-        }
-
-	//buat fungsi "GET"
-    function service_get()
     {
-        //ambil parameter token "(kode)"
-        $token = $this->get("kode");
-        
-        //panggil fungsi "get_data"
-        $hasil = $this->model->get_data(base64_encode($token));
+        parent::__construct();
+        // Your own constructor code
 
-        $this->response(array("lampu" =>
-        $hasil),200);
-
+        // // panggil model "Mlampu"
+        $this->load->model("Mlampu", "mdl", TRUE);
     }
 
-    //buat fungsi "POST"
+    // buat service "GET"
+    function service_get()
+    {
+        // ambil parameter token "kode"
+        $token = $this->get("kode");
+
+        // panggil method "get_data"
+        $hasil = $this->mdl->get_data(base64_encode($token));
+        // hasil respon
+        $this->response(array("lampu" => $hasil), 200);
+    }
+
+    // buat service "POST"
     function service_post()
     {
-       
-        //ambil parameter token data yang akan diisi
+        // // panggil model "Mlampu"
+        // $this->load->model("Mlampu","mdl",TRUE);
+        // ambil parameter data yang akan di isi
         $data = array(
-            "kode" => $this->post("kode"),
-            "nama" => $this->post("nama"),
-            "harga" => $this->post("harga"),
-            "tegangan" => $this->post("tegangan"),
+            "kode" => $this->post("kode"), //array $data[0]
+            "nama" => $this->post("nama"), //array $data[1]
+            "harga" => $this->post("harga"), //array $data[2]
+            "tegangan" => $this->post("tegangan"), //array $data[3]
             "token" => base64_encode($this->post("kode")),
         );
         // panggil method "save data"
-        $hasil = $this->model->save_data($data["kode"],
-        $data["nama"],$data["harga"],$data["tegangan"],$data["token"]);
+        $hasil = $this->mdl->save_data($data["kode"], $data["nama"], $data["harga"], $data["tegangan"], $data["token"]);
         // jika hasil = 0
-        if($hasil == 0 )
-        {
-            $this->response(array("status" =>"Data lampu Berhasil Disimpan"),200);
+        if ($hasil == 0) {
+            $this->response(array("status" => "Data Lampu Berhasil Disimpan"), 200);
         }
-        // jika hasil != 0
-        else
-        {
-            $this->response(array("status" =>"Data lampu Gagal Disimpan !"),200);
+        // jika hasil !=0
+        else {
+            $this->response(array("status" => "Data Lampu Gagal Disimpan"), 200);
         }
-
- 
     }
-    //buat fungsi "PUT"
+
+    // buat service "PUT"
     function service_put()
     {
-     //panggil model "Mlampu"
-     $this->load->model("Mlampu","model",true);
-     //ambil parameter token data yang akan diisi
-     $data = array(
-         "kode" => $this->put("kode"),
-         "nama" => $this->put("nama"),
-         "harga" => $this->put("harga"),
-         "tegangan" => $this->put("tegangan"),
-         "token" => base64_encode($this->put("token")),
-     );   
+        // // panggil model "Mlampu"
+        // $this->load->model("Mlampu","mdl",TRUE);
+        // ambil parameter data yang akan di isi
+        $data = array(
+            "kode" => $this->put("kode"), //array $data[0]
+            "nama" => $this->put("nama"), //array $data[1]
+            "harga" => $this->put("harga"), //array $data[2]
+            "tegangan" => $this->put("tegangan"), //array $data[3]
+            "token" => base64_encode($this->put("token")),
+        );
+        // panggil method "save data"
+        $hasil = $this->mdl->update_data($data["kode"], $data["nama"], $data["harga"], $data["tegangan"], $data["token"]);
 
-      // panggil method "update_data"
-      $hasil = $this->model->update_data($data
-      ["kode"],$data["nama"],$data["harga"],$data["tegangan"],$data["token"]);
-
-      //jika hasil == 0
-        if($hasil == 0 )
-        {
-            $this->response(array("status" =>"Data lampu Berhasil Diubah"),200);
+        // jika hasil = 0
+        if ($hasil == 0) {
+            $this->response(array("status" => "Data Lampu Berhasil Di Update"), 200);
         }
-        // jika hasil != 0
-        else
-        {
-            $this->response(array("status" =>"Data lampu Gagal Diubah !"),200);
+        // jika hasil !=0
+        else {
+            $this->response(array("status" => "Data Lampu Gagal Di Update"), 200);
         }
     }
-    //buat fungsi "DELETE"
+
+    // buat service "DELETE"
     function service_delete()
     {
-        // panggil model "Mlampu"
-        $this->load->model("Mlampu","model",TRUE);
-        //ambil parameter token "(kode)"
+        // // panggil model "Mlampu"
+        // $this->load->model("Mlampu","mdl",TRUE);
+        // ambil parameter token "kode"
         $token = $this->delete("kode");
-        //panggil fungsi "delete_data"
-        $hasil = $this->model->delete_data
-        (base64_encode($token));
-        if($hasil == 1)
-        {
-            $this->response(array("status" =>"Data lampu Berhasil Dihapus"),200);
+        //    panggil fungsi "delete_data"
+        $hasil = $this->mdl->delete_data(base64_encode($token));
+        if ($hasil == 1) {
+            $this->response(array("status" => "Data Lampu Berhasil Dihapus"), 200);
         }
         // jika proses delete gagal
-        else
-        {
-            $this->response(array("status" => "Data
-            lampu Gagal Dihapus !"),200);
+        else {
+            $this->response(array("status" => "Data Lampu Gagal Dihapus"), 200);
         }
-
     }
 }
--- Server/application/models/Mlampu.php
+++ Server/application/models/Mlampu.php
@@ -4,115 +4,108 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Mlampu extends CI_Model
 {
 
-  // buat method untuk tampil data
-  function get_data($token)
-  {
-    //mengambil hanya kode
-    //$this->db->select("kode")
+    // buat fungsi "get_data"
+    function get_data($token)
+    {
+        $this->db->select("id AS id_lampu, kode AS kode_lampu, nama AS nama_lampu, harga AS harga_lampu, tegangan AS tegangan_lampu");
+        $this->db->from("tb_lampu");
 
-    $this->db->select("id AS id_lampu,
-        kode AS kode_lampu,
-        nama AS nama_lampu,
-        harga AS harga_lampu,
-        tegangan AS tegangan_lampu
-        ");
+        // jika token terisi
+        if (!empty($token)) {
+            $this->db->where("TO_BASE64(kode) = '$token'");
+        }
 
-    $this->db->from("tb_lampu");
+        $this->db->order_by("kode", "ASC");
+        $query = $this->db->get()->result();
 
-    // jika token terisi
-    if ($token != "") {
-      $this->db->where("TO_BASE64(kode) = '$token' OR TO_BASE64(nama) = '$token' OR TO_BASE64(tegangan) = '$token'");
+        return $query;
+    }
+    // buat fungsi untuk delete data
+    function delete_data($token)
+    {
+        // cek apakah kode ada atau tidak
+        $this->db->select("kode");
+        $this->db->from("tb_lampu");
+        $this->db->where("TO_BASE64(kode) = '$token'");
+        // eksekusi query
+        $query = $this->db->get()->result();
+        // jika kode ditemukan
+        if (count($query) == 1) {
+            // hapus data lampu
+            $this->db->where("TO_BASE64(kode) = '$token'");
+            $this->db->delete("tb_lampu");
+            // kirim nilai hasil = 1
+            $hasil = 1;
+        }
+        // jika kode tidak di temukan
+        else {
+            // kirim nilai hasil =0
+            $hasil = 0;
+        }
+        // kirim variabel hasil ke "controler" Lampu
+        return $hasil;
     }
 
-    $this->db->order_by("kode", "ASC");
+    // buat fungsi untuk save data
+    function save_data($kode, $nama, $harga, $tegangan, $token)
+    {
+        // cek apakah kode ada atau tidak
+        $this->db->select("kode");
+        $this->db->from("tb_lampu");
+        $this->db->where("TO_BASE64(kode) = '$token'");
+        //  $this->db->where("kode = '$kode'");
+        // eksekusi query
+        $query = $this->db->get()->result();
+        // jika kode tidak ditemukan
+        if (count($query) == 0) {
+            // isi nilai untuk masing" filed
+            $data = array(
+                "kode" => $kode,
+                "nama" => $nama,
+                "harga" => $harga,
+                "tegangan" => $tegangan,
+            );
 
-    $query = $this->db->get()->result();
-    return $query;
-  }
+            // simmoan data
+            $this->db->insert("tb_lampu", $data);
+            $hasil = 0;
+        }
+        //  jika kode ditemukan 
+        else {
+            $hasil = 1;
+        }
+        return $hasil;
+    }
+    // fungsi untuk update data
+    function update_data($kode, $nama, $harga, $tegangan, $token)
+    {
+        // cek apakah kode ada atau tidak
+        $this->db->select("kode");
+        $this->db->from("tb_lampu");
+        $this->db->where("TO_BASE64(kode) != '$token' AND kode = '$kode'");
+        //  $this->db->where("kode = '$kode'");
+        // eksekusi query
+        $query = $this->db->get()->result();
+        // jika kode tidak ditemukan
+        if (count($query) == 0) {
+            // isi nilai untuk masing" filed
+            $data = array(
+                "kode" => $kode,
+                "nama" => $nama,
+                "harga" => $harga,
+                "tegangan" => $tegangan,
+            );
 
-  //buat fungsi untuk hapus data
-  function delete_data($token)
-  {
-    // cek apakah kode ada/tidak
-    $this->db->select("kode");
-    $this->db->from("tb_lampu");
-    $this->db->where("TO_BASE64(kode) = '$token'");
-    //eksekusi query
-    $query = $this->db->get()->result();
-    //jika kode ditemukan
-    if (count($query) == 1) {
-      // hapus data lampu
-      $this->db->where("TO_BASE64(kode) = '$token'");
-      $this->db->delete("tb_lampu");
-      // kirim nilai hasil = 1
-      $hasil = 1;
+            // hapus data lampu
+            $this->db->where("TO_BASE64(kode) = '$token'");
+            $this->db->update("tb_lampu", $data);
+            // kirim nilai hasil = 0
+            $hasil = 0;
+        }
+        //  jika kode di temukan
+        else {
+            $hasil = 1;
+        }
+        return $hasil;
     }
-    //jika kode tidak ditemukan 
-    else {
-      //kirim nilai hasil = 0
-      $hasil = 0;
-    }
-    // kirim variabel hasil ke "controller" lampu
-    return $hasil;
-  }
-
-  // buat fungsi untuk simpan data
-  function save_data($kode, $nama, $harga, $tegangan, $token)
-  {
-    // cek apakah kode ada/tidak
-    $this->db->select("kode");
-    $this->db->from("tb_lampu");
-    $this->db->where("TO_BASE64(kode) = '$token'");
-    //eksekusi query
-    $query = $this->db->get()->result();
-    //jika kode ditemukan
-    if (count($query) == 0) {
-      // isi nilai untuk masing2 field
-      $data = array(
-        "kode" => $kode,
-        "nama" => $nama,
-        "harga" => $harga,
-        "tegangan" => $tegangan,
-      );
-      // simpan data
-      $this->db->insert("tb_lampu", $data);
-      $hasil = 0;
-    }
-    // jika kode ditemukan
-    else {
-      $hasil = 1;
-    }
-    return $hasil;
-  }
-
-  //FUNGSI UNTUK UBAH DATA
-  function update_data($kode, $nama, $harga, $tegangan, $token)
-  {
-    // cek apakah kode ada/tidak
-    $this->db->select("kode");
-    $this->db->from("tb_lampu");
-    //$this->db->where("kode = '$kode'");
-    $this->db->where("TO_BASE64(kode) !='$token' AND kode = '$kode' ");
-    //eksekusi query
-    $query = $this->db->get()->result();
-    //jika kode ditemukan
-    if (count($query) == 0) {
-      // isi nilai untuk masing2 field
-      $data = array(
-        "kode" => $kode,
-        "nama" => $nama,
-        "harga" => $harga,
-        "tegangan" => $tegangan,
-      );
-
-      // ubah data lampu
-      $this->db->where("TO_BASE64(kode) = '$token'");
-      $this->db->update("tb_lampu", $data);
-      // kirim nilai hasil = 0
-      $hasil = 0;
-    } else {
-      $hasil = 1;
-    }
-    return $hasil;
-  }
 }
