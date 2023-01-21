7232b60295ff2392bd442992fdc8063e2694a7f0
ajipamungkas2 <aji_pamungkas@teknokrat.ac.id>
2023-01-21 14:45:36 +0700

    user1: Tambah Login, dengan JWT dari Server

--- Client-JWT/application/views/en_lampu.php
+++ Client-JWT/application/views/en_lampu.php
@@ -1,113 +1,107 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ubah Data Lampu</title>
-    <!-- import file "style.css" -->
-    <link rel="stylesheet" href="<?php echo base_url("ext/style.css") ?>" />
-</head>
+    <title>Tambah Data Lampu</title>
 
+    <!-- import file "style.css" -->
+    <link rel="stylesheet" href="<?php echo base_url("ext/style.css")?>" />
+</head>
 <body>
-    <!-- BUAT AREA MENU -->
+    <!-- buat area menu -->
     <nav class="area-menu">
         <button id="btn_lihat" class="btn-primary">Lihat Data</button>
         <button id="btn_refresh" class="btn-secondary" onclick="return setRefresh()">Refresh Data</button>
-
     </nav>
 
     <!-- buat area untuk entry data lampu -->
     <main class="area-grid">
         <section class="item-label1">
-            <label id="lbl_kode" for="txt_kode">KODE :</label>
+            <label id="lbl_kode" for="txt_kode">
+                KODE :
+            </label>
         </section>
         <section class="item-input1">
-            <input type="text" class="text-input" id="txt_kode" maxlength="9">
+            <input type="text" id="txt_kode" class="text-input" maxlength="9">
         </section>
         <section class="item-error1">
-            <p class="error-info" id="err_kode">Wajib Di Isi!!!</p>
+            <p id="err_kode" class="error-info"></p>
         </section>
 
         <section class="item-label2">
-            <label id="lbl_nama" for="txt_nama">Nama Lampu :</label>
+            <label id="lbl_nama" for="txt_nama">
+                Nama Lampu :
+            </label>
         </section>
         <section class="item-input2">
-            <input type="text" class="text-input" id="txt_nama" maxlength="100">
+            <input type="text" id="txt_nama" class="text-input" maxlength="100">
         </section>
         <section class="item-error2">
-            <p class="error-info" id="err_nama">Wajib Di Isi!!!</p>
+            <p id="err_nama" class="error-info"></p>
         </section>
 
         <section class="item-label3">
-            <label id="lbl_harga" for="txt_harga">Harga :</label>
+            <label id="lbl_harga" for="txt_harga">
+                Harga Lampu :
+            </label>
         </section>
         <section class="item-input3">
-            <input type="text" class="text-input" id="txt_harga" maxlength="15" onkeypress="return setNumber(event)">
+            <input type="text" id="txt_harga" class="text-input" maxlength="15" onkeypress="return setNumber(event)">
         </section>
         <section class="item-error3">
-            <p class="error-info" id="err_harga">Wajib Di Isi!!!</p>
+            <p id="err_harga" class="error-info"></p>
         </section>
 
         <section class="item-label4">
-            <label id="lbl_tegangan" for="txt_tegangan">Tegangan :</label>
+            <label id="lbl_tegangan" for="cbo_tegangan">
+                Tegangan Lampu :
+            </label>
         </section>
         <section class="item-input4">
-            <input type="text" class="text-input" id="cbo_tegangan" maxlength="15" onkeypress="return setNumber(event)">
+            <input type="text" id="cbo_tegangan" class="text-input" maxlength="15" onkeypress="return setNumber(event)">
         </section>
         <section class="item-error4">
-            <p class="error-info" id="err_tegangan">Wajib Di Isi!!!</p>
+            <p id="err_tegangan" class="error-info"></p>
         </section>
-
     </main>
-    <nav class="area-menu">
-        <button id="btn_ubah" class="btn-primary">Ubah Data</button>
-    </nav>
 
-    <!-- import file script.js -->
-    <script src="<?= base_url("ext/script.js") ?>"></script>
+    <!-- buat area menu -->
+    <nav class="area-menu" style="margin-top: 10px;">
+        <button id="btn_simpan" class="btn-primary">Simpan Data</button>        
+    </nav>
+    
+    <!-- import file "script.js" -->
+    <script src="<?php echo base_url("ext/script.js"); ?>"></script>
 
     <script>
-        // inisialisai object dan ambil data
-        let text_kode = document.getElementById("text_kode")
-        let text_nama = document.getElementById("text_nama")
-        let text_harga = document.getElementById("text_harga")
-        let cbo_tegangan = document.getElementById("cbo_tegangan")
-        let token = '<?= $token ?>'
-
-        txt_kode.value = '<?= $kode ?>'
-        txt_nama.value = '<?= $nama ?>'
-        txt_harga.value = '<?= $harga ?>'
-        cbo_tegangan.value = '<?= $tegangan ?>'
-
         // inisialisasi object
         let btn_lihat = document.getElementById("btn_lihat");
-        let btn_ubah = document.getElementById("btn_ubah");
+        let btn_simpan = document.getElementById("btn_simpan");
 
         //  buat event untuk "btn_lihat"
         btn_lihat.addEventListener('click', function() {
-            location.href = '<?php echo base_url(); ?>'
+            location.href = '<?php echo base_url("Lampu"); ?>'
         })
 
-        // buat fungsi set refresh
-        function setRefresh() {
-            location.href = '<?php echo site_url("Lampu/updateLampu") ?>' + '/' + token
-        }
-
-        //  buat event untuk "btn_ubah"
-        btn_ubah.addEventListener('click', function() {
+        //  buat event untuk "btn_simpan"
+        btn_simpan.addEventListener('click', function() {
             // inisialisasi objek
             let lbl_kode = document.getElementById("lbl_kode");
+            let txt_kode = document.getElementById("txt_kode");
             let err_kode = document.getElementById("err_kode");
 
             let lbl_nama = document.getElementById("lbl_nama");
+            let txt_nama = document.getElementById("txt_nama");
             let err_nama = document.getElementById("err_nama");
 
             let lbl_harga = document.getElementById("lbl_harga");
+            let txt_harga = document.getElementById("txt_harga");
             let err_harga = document.getElementById("err_harga");
 
             let lbl_tegangan = document.getElementById("lbl_tegangan");
+            let cbo_tegangan = document.getElementById("cbo_tegangan");
             let err_tegangan = document.getElementById("err_tegangan");
 
             // jika kode tidak di isi
@@ -168,44 +162,35 @@
             // Jika semua komponen terisi
             if (err_kode.innerHTML === "" && nama[1] === "" && harga[1] === "" && tegangan[1] === "") {
                 // alert("simpan")
-
                 // panggil method setSave
-                setUpdate(txt_kode.value, txt_nama.value, txt_harga.value, cbo_tegangan.value, token)
+                setSave(txt_kode.value, txt_nama.value, txt_harga.value, cbo_tegangan.value)
             }
         })
 
-        // buat fungsi setUpdate
-        const setUpdate = async (kode, nama, harga, tegangan, token) => {
+        // buat fungsi set save
+        const setSave = (kode, nama, harga, tegangan) => {
             // buat variable untuk form
             let form = new FormData()
-
             // isi/tambah nilai untuk form
             form.append("kodenya", kode)
             form.append("namanya", nama)
             form.append("harganya", harga)
             form.append("tegangannya", tegangan)
-            form.append("tokennya", token)
 
-            // async await
-            try {
-                // Proses kirim data ke controller
-                let response = await fetch('<?php echo site_url("Lampu/setUpdate") ?>', {
+            // proses kirm data ke controller
+            fetch('<?php echo site_url("Lampu/setSave") ?>', {
                     method: "POST",
                     body: form
                 })
-                // proses pembacaan hasil
-                let result = await response.json()
-                // tampilkan hasil
-                alert(result.statusnya)
+                .then((response) => response.json())
+                .then((result) => alert(result.statusnya))
+                .catch((error) => alert("Data gagal dikirim !!!"))
+        }
 
-
-            } catch {
-                alert("Data gagal dikirim !!!")
-            }
-
+        // buat fungsi set refresh
+        function setRefresh() {
+            location.href = '<?php echo site_url("Lampu/addLampu") ?>';
         }
     </script>
-
 </body>
-
 </html>
